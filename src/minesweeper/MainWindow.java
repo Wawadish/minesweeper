47ba9549e536b77117d771b92efb7c39331fd951
TheBerg974 <32581690+TheBerg974@users.noreply.github.com>
2018-09-15 18:19:51 -0400

    Update!
    Sounds, Optimizing

--- src/minesweeper/MainWindow.java
+++ src/minesweeper/MainWindow.java
@@ -1,14 +1,19 @@
 package minesweeper;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.*;
-import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 
 /**
@@ -16,6 +21,9 @@ import javax.swing.*;
  */
 public class MainWindow extends JFrame implements KeyListener {
 
+    //Sound Files
+    private File winSound = new File("C:\\Users\\Samuel\\Documents\\GitHub\\minesweeper\\src\\minesweeper\\america-fuck-yeah-ultimate-edition-1.WAV");
+    private File lostSound = new File("C:\\Users\\Samuel\\Documents\\GitHub\\minesweeper\\src\\minesweeper\\wtf_boom.WAV");
 
     private int mineCounter = 0;
 
@@ -35,13 +43,13 @@ public class MainWindow extends JFrame implements KeyListener {
         minesweepermenu.gameInProgress.setText("Game is in progress...");
 
         this.addKeyListener(this);
-        
+
         //Making the window a square, that will fit 8 square buttons (80px Height and Width).
         setSize(640, 640);
 
         //Creating the buttons in the buttons array(8x8)
         this.setLocationRelativeTo(null);
-        
+
         this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSED));
 
         //creating a container and retrieve the MainWindow's content pane from it.
@@ -60,8 +68,9 @@ public class MainWindow extends JFrame implements KeyListener {
                 minesweepermenu.numberGamesLost++;
                 minesweepermenu.gamesLost.setText("Games Lost:\n" + minesweepermenu.numberGamesLost);
                 minesweepermenu.gameInProgress.setText("The game is done. You lost!");
-            }  
+            }
         });
+
         //Making all Blocks in the blocks array Empty Blocks.
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
@@ -84,7 +93,6 @@ public class MainWindow extends JFrame implements KeyListener {
             }
         }
 
-
         //Starting two big loops, the variables i and j will be used by both blocks array and buttons array.
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
@@ -101,96 +109,47 @@ public class MainWindow extends JFrame implements KeyListener {
                 }
 
                 //Making the buttons 80px x 80px to fit the main window size.
-
                 buttons[i][j].setSize(80, 80);
                 buttons[i][j].setFocusable(false);
                 final int row = i;
                 final int column = j;
 
                 //Setting the event which will happen once the button is clicked.
-
                 buttons[i][j].addActionListener((e) -> {
 
                     if (!keyPressed) {
-
                         //This code is executed every time a button  is clicked.
                         //Disabling the button.
-                        if(!buttons[row][column].getText().equals("Flag")){
-                        buttons[row][column].setEnabled(false);
-                        //Depending on which type of block was clicked, execute a different command
-                        switch (blocks[row][column]) {
+                        if (!buttons[row][column].getText().equals("Flag")) {
+                            buttons[row][column].setEnabled(false);
+                            
                             //For number blocks, set the block text to whichever number it is.
-                            case ONE:
-                                buttons[row][column].setText("1");
-                                break;
-                            case TWO:
-                                buttons[row][column].setText("2");
-                                break;
-                            case THREE:
-                                buttons[row][column].setText("3");
-                                break;
-                            case FOUR:
-                                buttons[row][column].setText("4");
-                                break;
-                            case FIVE:
-                                buttons[row][column].setText("5");
-                                break;
-                            case SIX:
-                                buttons[row][column].setText("6");
-                                break;
-                            case SEVEN:
-                                buttons[row][column].setText("7");
-                                break;
-                            case EIGHT:
-                                buttons[row][column].setText("8");
-                                break;
-                            case EMPTY:
-                                emptyCheck(row, column);
-                                break;
-                            case MINE:
+                            setTextButton(blocks, row, column);
+                            //Depending on which type of block was clicked, execute a different command
+                            switch (blocks[row][column]) {
+                                //For number blocks, set the block text to whichever number it is.
+                                case EMPTY:
+                                    emptyCheck(row, column);
+                                    break;
+                                case MINE:
+                                    // If the block you clicked on is a mine, update the game menu and get rid of this frame.
+                                    minesweepermenu.numberGamesLost++;
+                                    minesweepermenu.gamesLost.setText("Games Lost:\n" + minesweepermenu.numberGamesLost);
+                                    minesweepermenu.gameInProgress.setText("The game is done. You lost!");
+                                    this.dispose();
+                                    playSound(lostSound);
+                            }
+                        }
 
-                                // If the block you clicked on is a mine, update the game menu and get rid of this frame.
-                                minesweepermenu.numberGamesLost++;
-                                minesweepermenu.gamesLost.setText("Games Lost:\n" + minesweepermenu.numberGamesLost);
-                                minesweepermenu.gameInProgress.setText("The game is done. You lost!");
-                                this.dispose();
-                        }
-                        }
-                        
                     } else {
-                        if(DEBUG_MODE && buttons[row][column].getText().equals("Flag") && blocks[row][column] == Block.MINE){
+                        if (DEBUG_MODE && buttons[row][column].getText().equals("Flag") && blocks[row][column] == Block.MINE) {
                             buttons[row][column].setText("Mine");
                             return;
-                        }else if( buttons[row][column].getText().equals("Flag")){
-                            switch(blocks[row][column]){
-                            case ONE:
-                                buttons[row][column].setText("1");
-                                break;
-                            case TWO:
-                                buttons[row][column].setText("2");
-                                break;
-                            case THREE:
-                                buttons[row][column].setText("3");
-                                break;
-                            case FOUR:
-                                buttons[row][column].setText("4");
-                                break;
-                            case FIVE:
-                                buttons[row][column].setText("5");
-                                break;
-                            case SIX:
-                                buttons[row][column].setText("6");
-                                break;
-                            case SEVEN:
-                                buttons[row][column].setText("7");
-                                break;
-                            case EIGHT:
-                                buttons[row][column].setText("8");
-                                break;
-                            case EMPTY:
-                                buttons[row][column].setText("");
-                            }
-                        }else{
+                        } else if (buttons[row][column].getText().equals("Flag") && !DEBUG_MODE) {
+                            buttons[row][column].setText("");
+                        } else if(buttons[row][column].getText().equals("Flag") && DEBUG_MODE) {
+                            setTextButton(blocks, row, column);
+                        }else {
                             buttons[row][column].setText("Flag");
                         }
                     }
@@ -199,33 +158,10 @@ public class MainWindow extends JFrame implements KeyListener {
 
                 //Adding the buttons to the Main Window
                 this.add(buttons[i][j]);
+
+                //Displaying Text on buttons for Debug Mode
                 if (DEBUG_MODE) {
-                    switch (blocks[i][j]) {
-                        case ONE:
-                            buttons[i][j].setText("1");
-                            break;
-                        case TWO:
-                            buttons[i][j].setText("2");
-                            break;
-                        case THREE:
-                            buttons[i][j].setText("3");
-                            break;
-                        case FOUR:
-                            buttons[i][j].setText("4");
-                            break;
-                        case FIVE:
-                            buttons[i][j].setText("5");
-                            break;
-                        case SIX:
-                            buttons[i][j].setText("6");
-                            break;
-                        case SEVEN:
-                            buttons[i][j].setText("7");
-                            break;
-                        case EIGHT:
-                            buttons[i][j].setText("8");
-                            break;
-                    }
+                    setTextButton(blocks, i, j);
                 }
 
                 //The two following brackets mark the end of the two big for loops
@@ -264,13 +200,13 @@ public class MainWindow extends JFrame implements KeyListener {
     /*The following method is recursive and disables adjacent Empty or Number Blocks, it will only use recursion if
       it's inputted Block is an Empty (Not a Number Block).*/
     public void check(int x, int y) {
-        if ((x != 8) && (y != -1) && (x != -1) && (y != 8) && (blocks[x][y] == Block.ONE || blocks[x][y] == Block.TWO ||
-                blocks[x][y] == Block.THREE || blocks[x][y] == Block.FOUR || blocks[x][y] == Block.FIVE
+        if ((x != 8) && (y != -1) && (x != -1) && (y != 8) && (blocks[x][y] == Block.ONE || blocks[x][y] == Block.TWO
+                || blocks[x][y] == Block.THREE || blocks[x][y] == Block.FOUR || blocks[x][y] == Block.FIVE
                 || blocks[x][y] == Block.SIX || blocks[x][y] == Block.SEVEN || blocks[x][y] == Block.EIGHT)) {
             buttons[x][y].setEnabled(false);
             buttons[x][y].setText(getBlockString(blocks[x][y]));
         }
-        if ((x != 8) && (x != -1) && (y != 8) && (y != -1) && blocks[x][y] == Block.EMPTY && buttons[x][y].isEnabled()){
+        if ((x != 8) && (x != -1) && (y != 8) && (y != -1) && blocks[x][y] == Block.EMPTY && buttons[x][y].isEnabled()) {
             buttons[x][y].setEnabled(false);
             emptyCheck(x, y);
         }
@@ -315,7 +251,7 @@ public class MainWindow extends JFrame implements KeyListener {
 
         /*The following statements will set the given Block (X , Y) to a block representing the amount of
           surrounding Mine Blocks.*/
-        if(blocks[x][y] != Block.MINE) {
+        if (blocks[x][y] != Block.MINE) {
             switch (tempCounter) {
                 case 1:
 
@@ -384,27 +320,72 @@ public class MainWindow extends JFrame implements KeyListener {
         }
     }
     
-    public void checkWin(){
+    //This method adds text on a button
+    public void setTextButton(Block[][] blocks, int x, int y) {
+        switch (blocks[x][y]) {
+            case ONE:
+                buttons[x][y].setText("1");
+                break;
+            case TWO:
+                buttons[x][y].setText("2");
+                break;
+            case THREE:
+                buttons[x][y].setText("3");
+                break;
+            case FOUR:
+                buttons[x][y].setText("4");
+                break;
+            case FIVE:
+                buttons[x][y].setText("5");
+                break;
+            case SIX:
+                buttons[x][y].setText("6");
+                break;
+            case SEVEN:
+                buttons[x][y].setText("7");
+                break;
+            case EIGHT:
+                buttons[x][y].setText("8");
+                break;
+            case EMPTY:
+                buttons[x][y].setText("");
+
+        }
+
+    }
+    
+    //This method checks if the winning conditions are attained
+    public void checkWin() {
         int winCounter = 0;
-        for(int h = 0; h < 8; h++){
+        for (int h = 0; h < 8; h++) {
             for (int k = 0; k < 8; k++) {
-                if(!buttons[h][k].isEnabled()){
+                if (!buttons[h][k].isEnabled()) {
                     winCounter++;
-                    System.out.println(winCounter);
-                } 
+                }
             }
         }
-        if(winCounter == 54){
+        if (winCounter == 54) {
             minesweepermenu.numberGamesWon++;
             minesweepermenu.gamesWon.setText("Games Won:\n" + minesweepermenu.numberGamesWon);
             minesweepermenu.gameInProgress.setText("The game is done. You won!");
+            playSound(winSound);
             this.dispose();
+        }
+    }
+
+    //Method to play winning and losing sound
+    public void playSound(File sound) {
+        try {
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(sound));
+            clip.start();
+        } catch (IOException | LineUnavailableException | UnsupportedAudioFileException e) {
         }
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-
+        
     }
 
     @Override
